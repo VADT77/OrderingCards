e94bc22b3a3b9795b56500e43b7845c9b9669641
VADT77 <turanosoff@gmail.com>
2023-03-02 16:48:19 +0300

    More tests

--- src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
@@ -45,4 +45,41 @@ public class AppOrderPositiveTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
+    @Test
+    void test2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Ivanov Ivan");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79012345678");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("button[role='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'] span[class='input__sub']")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+    @Test
+    void test3() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Пантелеймон Пантелеймонов");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79012345");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("button[role='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'] span[class='input__sub']")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+    @Test
+    void test4() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Пантелеймон Пантелеймонов");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79012345678");
+//        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector("button[role='button']")).click();
+        String text = driver.findElement(By.cssSelector(".input_invalid")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+}
 
